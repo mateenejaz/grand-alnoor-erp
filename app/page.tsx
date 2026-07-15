e505f5b610f9e433bf2c94ef835258fc54ba1932
mateenejaz <mateen.ejaz@gmail.com>
2026-07-15 19:58:48 +0500

    Add home page redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
 export default function RootPage() {
-  // Automatically send anyone visiting the home page to the secure dashboard
+  // This sends anyone visiting the home page straight to the dashboard
   redirect('/dashboard');
 }
